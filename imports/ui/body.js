9b313dad60cf7827f89b668d500d8f00b1726808
Lukas Navickas <luk.navickas@gmail.com>
2016-05-09 15:49:32 +0200

    added materialize

---
+++ imports/ui/body.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { materialize } from 'meteor/materialize:materialize';
+
+import './body.html';
+import './navbar.html';
